bcd75799d015c99137c8d52e53984497dea71e7f
benjaminthorent <b.thorent@gmail.com>
2011-03-24 14:08:37 +0100

    Commit after Nelle's pull

--- app/controllers/plagiarism_controller.rb
+++ app/controllers/plagiarism_controller.rb
@@ -1,8 +1,42 @@
 class PlagiarismController < ApplicationController
 
+  # Displays "Manage Plagiarism Config" page for creating 
+  # plagiarism configuration form
   def new
     @assignments = Assignment.find(:all)
     @plagiarism_config = PlagiarismConfig.new
+  end
+  
+  # Displays "Manage Plagiarism Config" page for editing 
+  # plagiarism configuration form
+  def edit
+    @assignment = Assignment.find_by_id(params[:id])
+    if !request.post?
+      return
+    end
+  
+    begin
+      @plagiarism_config = process_assignment_form(@plagiarism_config, params)
+      rescue Exception, RuntimeError => e
+        @plagiarism_config.errors.add_to_base(I18n.t("plagiarismerror", 
+                                              :message => e.message))
+      return
+    end
+    
+    if @plagiarism_config.save
+      flash[:success] = I18n.t("assignment.update_success")
+      redirect_to :action => 'edit', :id => params[:id]
+      return
+    else
+      render :action => 'edit'
+    end
+  end
+ 
+  # Method to manage form processing
+  def process_assignment_form(plagiarism_config, params)
+    plagiarism_config.attributes = params[:plagiarism_config] 
+    
+    return plagiarism_config
   end
 
 end
